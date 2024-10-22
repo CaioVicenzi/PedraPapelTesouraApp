50880d87cba45e87c9f2a8e3f5612b97f3ef2388
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-10-21 23:30:09 -0300

    .

--- App.js
+++ App.js
@@ -1,13 +1,98 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [escolha, setEscolha] = useState('')
+  const [resultado, setResultado] = useState('')
+  const [escolhaAdversario, setEscolhaAdversario] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Pedra, papel, ou tesoura!</Text>
+
+
+      <View style={styles.hStack}>
+        <Text style={styles.botaoOpcao} onPress={escolherPedra}>🪨</Text>
+        <Text style={styles.botaoOpcao} onPress={escolherPapel}>📃</Text>
+        <Text style={styles.botaoOpcao} onPress={escolherTesoura}>✂️</Text>
+      </View>
+
+      { escolha != '' ? (
+        <Text>{`Você escolheu ${escolha }`}</Text>
+      ) : null
+
+      }
+
+      {
+        escolhaAdversario != '' ? (
+          <View>
+            <Text>{`Adversário escolheu: ${escolhaAdversario }`}</Text>
+            <Text>{`Resultado: ${resultado }`}</Text>
+            <Button title='Limpar' onPress={limpar}></Button>
+          </View>  
+       ) : <Button title='jogar' onPress={jogar}></Button>
+      }
+
+
       <StatusBar style="auto" />
     </View>
   );
+
+  function escolherPedra () {
+    setEscolha('🪨')
+  }
+
+  function escolherPapel () {
+    setEscolha('📄')
+  }
+
+  function escolherTesoura () {
+    setEscolha('✂️')
+  }
+
+  function limpar () {
+    setEscolhaAdversario('')
+    setEscolha('')
+    setResultado('')
+  }
+
+  function jogar () {
+    if (escolha != '') {
+
+    const opcoes = ["🪨", "📄", "✂️"]
+    const jogadaAdversario = opcoes[Math.floor(Math.random() * opcoes.length)]
+
+    if (jogadaAdversario == escolha) {
+      setResultado('Foi um empate')
+    } else if (escolha == '🪨') {
+      if (jogadaAdversario == '📄') {
+        setResultado('Você perdeu...')
+      } else {
+        setResultado('Você ganhou!')
+      }
+    } else if (escolha == '📄') {
+      if (jogadaAdversario == '✂️') {
+        setResultado('Você perdeu...')
+      } else {
+        setResultado('Você ganhou!')
+      }
+    } else {
+      if (jogadaAdversario == '🪨') {
+        setResultado('Você perdeu...')
+      } else {
+        setResultado('Você ganhou!')
+      }
+    }
+
+    setEscolhaAdversario(jogadaAdversario)
+  } else {
+    alert('Não escolheu nenhuma opção!')
+  }
+
+  }
+
+  
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +102,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title : {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingBottom: 30
+  },
+  hStack: {
+    flexDirection: 'row'
+  },
+
+  botaoOpcao: {
+    fontSize: 42,
+    padding: 30
+  }
+
 });
